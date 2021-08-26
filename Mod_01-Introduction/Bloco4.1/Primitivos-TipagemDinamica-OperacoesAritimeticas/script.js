3c2633902610b2102453a30631933782726f2156
TjAndrade99 <tjandrade0910@gmail.com>
2021-08-26 13:40:17 -0300

    att ifelse

--- Mod_01-Introduction/Bloco4.1/Primitivos-TipagemDinamica-OperacoesAritimeticas/script.js
+++ Mod_01-Introduction/Bloco4.1/Primitivos-TipagemDinamica-OperacoesAritimeticas/script.js
@@ -1,7 +1,40 @@
-let patientId = 50;
-let isEnrolled = true;
-const patientInfo = {
-  firstName: 'Ana',
-  lastName: 'Santos',
-};
-const patientEmail = 'ana@email.com';
+// let patientId = "50";
+// let isEnrolled = true;
+// const patientInfo = {
+//   firstName: 'Ana',
+//   lastName: 'Santos',
+// };
+// const patientEmail = 'ana@email.com';
+
+
+// console.log(typeof patientId)
+// console.log(typeof isEnrolled)
+// console.log(typeof patientInfo)
+// console.log(typeof patientEmail)
+
+// console.log(typeof patientAge)
+
+
+const base = 5;
+let height = 8;
+const area = base * height
+const perimeter = area + base + height
+
+console.log(area)
+console.log(perimeter)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Mod_01-Introduction/Bloco4.1/condicoes-If-Else/script.js
@@ -0,0 +1,42 @@
+// if = se
+// else = senão
+
+// ---------------------------------------------------------------------
+// ---------------------------------------------------------------------
+
+
+// let trybe = 16
+
+// if (trybe <= 14 && trybe < 14.40){
+//     console.log("esquenta")
+// }
+// else if (trybe <= 16 && trybe < 18){
+//     console.log("Aula ao vivo")
+// }
+// else if (trybe <= 19.30 && trybe > 20){
+//     console.log("Fechamento")
+// }
+// else {
+//     console.log("Fora dos momentos síncronos")
+// }
+
+
+// ---------------------------------------------------------------------
+// ---------------------------------------------------------------------
+
+const notaCandidato = 50
+
+if (notaCandidato >= 80 ){
+    console.log("Parabéns você foi aprovado(a)")
+}
+else if (notaCandidato < 80 && notaCandidato >= 60){
+    console.log("Você está na nossa lista de espera")
+}
+else console.log("Você foi reprovado(a)")
+
+
+
+
+
+
+
